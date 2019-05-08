cdc43c0d63897f7de79da27330232411e2037dea
Vladimir Gavrilov <vladimir.gavrilov@hotmail.com>
2019-05-08 15:08:06 +0300

    login.php updated

--- php/login.php
+++ php/login.php
@@ -5,3 +5,15 @@
  * Date: 8.5.2019
  * Time: 13.35
  */
+include("dbconnect.inc.php");
+
+$connection = new Connection();
+
+    if($connection->getUser($_GET['email']) == true AND $connection->getPass($_GET['pwd']) == true){
+        $_SESSION['admin'] = $admin;
+
+        header("Location: ../index.html");
+        exit;
+    }else echo '<p>Login or password is not corect</p>';
+
+?>
