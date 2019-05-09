123e3fbb264b5020c0dd40be09e786e762f582e8
Vladimir Gavrilov <vladimir.gavrilov@hotmail.com>
2019-05-10 00:19:45 +0300

    login warning is updated

--- php/login.php
+++ php/login.php
@@ -1,3 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Webstore | Log In</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../css/stylesheet.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="top">
+    <ul class="ultop">
+        <li id="main"><a id="home" href="../index.php">Webstore</a></li>
+        <ul class="row">
+            <ul>
+                <li id="login"><a href="../login.html">Log in</a></li>
+            </ul>
+</nav>
+<div class="container">
 <?php
 session_start();
 /**
@@ -16,6 +37,11 @@ $connection = new Connection();
 
         header("Location: ../index.php");
         exit;
-    }else echo '<p>Login or password is not corect</p>';
+    }else echo '<div class="alert alert-warning"><strong>Warning!</strong> Login or password is not corect!</div>';
 
 ?>
+
+</div>
+</body>
+</html>
+
