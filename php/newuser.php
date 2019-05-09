1826b8c5387f979c07758ba840f699883da8d2f8
Vladimir Gavrilov <vladimir.gavrilov@hotmail.com>
2019-05-09 14:13:29 +0300

    create user sivu on lisätty

--- php/newuser.php
+++ php/newuser.php
@@ -9,11 +9,12 @@ include("dbconnect.inc.php");
 
 $connection = new Connection();
 
-if($connection->getUser($_GET['email']) == true AND $connection->getPass($_GET['pwd']) == true){
-    $_SESSION['admin'] = $admin;
+if($connection->getUser($_GET['email']) == true ){
+    echo '<div class=/"alert alert-info/"><strong>Info!</strong> The username is already exsists.</div>';
+}else {
+    $connection->createUser($_GET['email'], $_GET['firstname'], $_GET['lastname'], $_GET['pwd']);
+    echo '<div class=/"alert alert-success/"><strong>Success!</strong> The username is created.</div>';
+}
 
-    header("Location: ../index.html");
-    exit;
-}else echo '<p>Login or password is not corect</p>';
 
 ?>
