d425d0ca7cc5754c976aecedbc66741d0289ad0b
Paavo Helin <paavodotcom@gmail.com>
2019-05-10 09:42:54 +0300

    Delete getCategories.js

--- js/getCategories.js
+++
@@ -1,8 +0,0 @@
-var categories = document.getElementById('categories');
-    for(var i=0; i < 5; i++) {
-            var row = document.createElement('a');
-            row.setAttribute('href', '#juu');
-            row.innerHTML = 'Juuuuuu </br>';
-            categories.appendChild(row);
-
-    }
