979bd0a7dce1924a00a58fc703d1b765bc004daf
Paavo Helin <paavodotcom@gmail.com>
2019-05-10 10:05:45 +0300

    Update newuser.php

--- php/newuser.php
+++ php/newuser.php
@@ -30,10 +30,10 @@
 include("dbconnect.inc.php");
 
 $connection = new Connection();
-
+//kertoo jos tunnus on jo olemassa
 if($connection->getUser($_GET['email']) == true ){
     echo '<div class="alert alert-info"><strong>Info!</strong> The username is already exsists.</div>';
-}else {
+}else { //ilmoittaa että tunnuseten tekeminen onnistui
     $connection->createUser($_GET['email'], $_GET['firstname'], $_GET['lastname'], $_GET['pwd']);
     $_SESSION['user'] = $_GET['email'];
     echo '<div class="alert alert-success"><strong>Success!</strong> The username is created.</div>';
