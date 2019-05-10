1d2dfb083f7ab064e0a2ba8e7ba0b66b31d6cda4
Paavo Helin <paavodotcom@gmail.com>
2019-05-10 10:03:30 +0300

    Update dbqueries.php

--- php/dbqueries.php
+++ php/dbqueries.php
@@ -8,18 +8,7 @@ $conn = new Connection();
 if (!isset($_SESSION["cart"])){
     $_SESSION["cart"] = array();
 }
-/*
-$sql = "SELECT Name FROM Category";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "Name: " . $row["Name"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
- */
+//vaihtaa functiota index.js suoritettujen functioiden mukaan
 switch ($func) {
     case getCategories:
         echo $conn->getCategories();
